be28b1813cb19f326fec22e6fbce176060ab5749
Trevor DePew <t.depewmusic@gmail.com>
2023-02-08 17:01:03 -0500

    all menu cards are in place and ready for content - need to set up rest of pages

--- user-interface/src/pages/HomePage.jsx
+++ user-interface/src/pages/HomePage.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
-import { Navbar } from "../app/Navbar";
-import { MenuItem } from "../features/modals/MenuItem";
+import { useNavigate } from "react-router-dom";
 import '../../src/styling/homepage.css'
 
 const HomePage = () => {
-  const [user, setuser] = useState(null)
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+
   useEffect(() => {
     const loadUser = async () => {
       let req = await fetch("http://localhost:3000/me", {
         headers: { Authorization: Cookies.get('token') }
       })
       let res = await req.json()
-      if (res.user) setuser(res.user)
+      if (res.user) setUser(res.user)
     }
     if (Cookies.get('token')) loadUser()
   }, [])
@@ -38,6 +39,12 @@ const HomePage = () => {
     }
   }
 
+  const logout = () => {
+    Cookies.remove('token')
+    navigate("/login")
+    setUser(null)
+  }
+
   const openNav = () => {
     document.getElementById("sideNav").style.width = "250px";
   }
@@ -51,6 +58,30 @@ const HomePage = () => {
 
   const closeAccount = () => {
     document.getElementById("account").style.width = "0";
+  }
+
+  const openAbout = () => {
+    document.getElementById("about").style.width = "750px";
+  }
+
+  const closeAbout = () => {
+    document.getElementById("about").style.width = "0";
+  }
+
+  const openNew = () => {
+    document.getElementById("new").style.width = "750px";
+  }
+
+  const closeNew = () => {
+    document.getElementById("new").style.width = "0";
+  }
+
+  const openOpen = () => {
+    document.getElementById("open").style.width = "750px";
+  }
+
+  const closeOpen = () => {
+    document.getElementById("open").style.width = "0";
   }
 
   return (
@@ -70,23 +101,23 @@ const HomePage = () => {
         </div>
         <a href="javascript:void(0)" className="closeBtn" onClick={closeNav}>&times;</a>
         <a onClick={openAccount}>Account</a>
-        <div id="account">
+        <div id="account" className="menu-card">
           <a href="javascript:void(0)" className="closeBtn" onClick={closeAccount}>&times;</a>
           <h1>ACCOUNT</h1>
           <div id="account-container">
             <div id="details-container">
               <div id="avatar">
-                <img src={user.avatar}/>
+                <img src="#" />user.avatar
               </div>
               <div id="details">
                 <div className="detail">
-                  <p>Username: {user.username} <button>edit</button></p>
+                  <p>Username: user.username <button>edit</button></p>
                 </div>
                 <div className="detail">
-                  <p>Email: {user.email} <button>edit</button></p>
+                  <p>Email: user.email <button>edit</button></p>
                 </div>
                 <div className="detail">
-                  <p>Password: {user.password} <button>edit</button></p>
+                  <p>Password: user.password <button>edit</button></p>
                 </div>
               </div>
             </div>
@@ -95,14 +126,81 @@ const HomePage = () => {
                 <button id="manage-btn">Manage</button>
               </div>
               <div >
-                <button id="logout-btn">Logout</button>
+                <button id="logout-btn" onClick={logout}>Logout</button>
               </div>
           </div>
           </div>
         </div>
-        <a >About</a>
-        <a href="#">New Project</a>
-        <a href="#">Open Project</a>
+        <a onClick={openAbout}>About</a>
+          <div id="about" className="menu-card">
+            <a href="javascript:void(0)" className="closeBtn" onClick={closeAbout}>&times;</a>
+            <h1>ABOUT</h1>
+            <div id="about-container" className="menu-card-container">
+              <div id="details-container">
+                <div id="tad">
+                  <div id="tad-img">
+                    <img src="../src/assets/avatar6.png" />
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam elit, 
+                    rutrum quis dolor vel, porttitor dignissim nisl. Mauris quis pellentesque justo, 
+                    ac mattis purus. Maecenas finibus velit malesuada, lobortis elit eget, hendrerit enim. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque lectus 
+                    quis mauris pulvinar, sodales consequat mauris elementum. Cras non sapien felis. 
+                    In vitae tortor sit amet orci venenatis porta ac laoreet nulla. Phasellus hendrerit 
+                    nunc ut auctor pellentesque. Proin sit amet sem porttitor, semper quam nec, tincidunt 
+                    ante. Praesent odio felis, varius a diam sed, lacinia egestas ipsum.</p>
+                </div>
+              </div>
+              <div id="links-container">
+                <a href="#">Synths</a>
+                <a href="#">Tone.js</a>
+              </div>
+            </div>
+          </div>
+        <a onClick={openNew}>New Project</a>
+          <div id="new" className="menu-card">
+            <a href="javascript:void(0)" className="closeBtn" onClick={closeNew}>&times;</a>
+            <h1>NEW PROJECT</h1>
+            <div id="new-container" className="menu-card-container">
+              <div id="details-container">
+                <div id="tad">
+                  <div id="tad-img">
+                    <img src="../src/assets/avatar6.png" />
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam elit,
+                    rutrum quis dolor vel, porttitor dignissim nisl. Mauris quis pellentesque justo,
+                    ac mattis purus. Maecenas finibus velit malesuada, lobortis elit eget, hendrerit enim.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque lectus
+                    quis mauris pulvinar, sodales consequat mauris elementum. Cras non sapien felis.
+                    In vitae tortor sit amet orci venenatis porta ac laoreet nulla. Phasellus hendrerit
+                    nunc ut auctor pellentesque. Proin sit amet sem porttitor, semper quam nec, tincidunt
+                    ante. Praesent odio felis, varius a diam sed, lacinia egestas ipsum.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        <a onClick={openOpen}>Open Project</a>
+          <div id="open" className="menu-card">
+            <a href="javascript:void(0)" className="closeBtn" onClick={closeOpen}>&times;</a>
+            <h1>OPEN PROJECT</h1>
+            <div id="open-container" className="menu-card-container">
+              <div id="details-container">
+                <div id="tad">
+                  <div id="tad-img">
+                    <img src="../src/assets/avatar6.png" />
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam elit,
+                    rutrum quis dolor vel, porttitor dignissim nisl. Mauris quis pellentesque justo,
+                    ac mattis purus. Maecenas finibus velit malesuada, lobortis elit eget, hendrerit enim.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque lectus
+                    quis mauris pulvinar, sodales consequat mauris elementum. Cras non sapien felis.
+                    In vitae tortor sit amet orci venenatis porta ac laoreet nulla. Phasellus hendrerit
+                    nunc ut auctor pellentesque. Proin sit amet sem porttitor, semper quam nec, tincidunt
+                    ante. Praesent odio felis, varius a diam sed, lacinia egestas ipsum.</p>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   )
