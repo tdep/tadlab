a1f9af444383fe3aaa00d01dd6858ff3cb469c79
Trevor DePew <t.depewmusic@gmail.com>
2023-02-13 01:07:48 -0500

    added ids to the keys

--- user-interface/src/components/interface_components/Keys.jsx
+++ user-interface/src/components/interface_components/Keys.jsx
@@ -5,6 +5,14 @@ export const Keys = () => {
   const fToBWhite = [4, 5, 6, 7]
   const fToBBlack = [1, 2, 3]
 
+
+  let i = 1
+  let j = 1
+  let notes = {
+    naturals: {1: 'C', 2: 'D', 3: 'E', 4: 'F', 5: 'G', 6: 'A', 7:'B'},
+    sharps: {1:'C#', 2:'D#', 4:'F#', 5:'G#', 6:'A#'}
+  }
+
   const whiteKey = {
     id: "",
     name: "",
@@ -22,15 +30,32 @@ export const Keys = () => {
       <div className="cToE">
         <div className="white">
           {cToEWhite.map((key) => {
+            const keyId = notes.naturals[i]
+            i += 1
             return (
-              <div className="cToEWhite"></div>
+              <div 
+              className="cToEWhite"
+              id={keyId}
+              ></div>
             )
           })}
         </div>
         <div className="black">
           {cToEBlack.map((key) => {
+            let keyId
+            if (j != 3) {
+              keyId = notes.sharps[j]
+              j += 1
+            } else {
+              j = 4
+              keyId = notes.sharps[j]
+              j += 1
+            }
             return (
-              <div className="cToEBlack"></div>
+              <div 
+              className="cToEBlack"
+              id={keyId}
+              ></div>
             )
           })}
         </div>
@@ -39,15 +64,32 @@ export const Keys = () => {
       <div className="fToB">
         <div className="white">
           {fToBWhite.map((key) => {
+            const keyId = notes.naturals[i]
+            i += 1
             return (
-              <div className="fToBWhite"></div>
+              <div 
+              className="fToBWhite"
+              id={keyId}
+              ></div>
             )
           })}
         </div>
         <div className="black">
           {fToBBlack.map((key) => {
+            let keyId
+            if (j != 3) {
+              keyId = notes.sharps[j]
+              j += 1
+            } else {
+              j = 4
+              keyId = notes.sharps[j]
+              j += 1
+            }
             return (
-              <div className="fToBBlack"></div>
+              <div 
+              className="fToBBlack"
+              id={keyId}
+              ></div>
             )
           })}
         </div>
